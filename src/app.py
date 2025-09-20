85fdcb4f4481b7f92d6575c44fe7fa9f6060796c
Kaleb Williams <kalebklw@gmail.com>
2025-09-20 01:50:36 +0000

    added the functionality to add a favorite character

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planet, Characters
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,53 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
+@app.route('/users', methods=['GET'])
 def handle_hello():
-
+    users = User.query.all()
+    users_list = [item.serialize() for item in users]
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "msg": "Hello, this is your GET /users response ",
+        "users": users_list
     }
 
     return jsonify(response_body), 200
+
+@app.route('/characters', methods=['GET'])
+def added_characters():
+    characters = Characters.query.all()
+    characters_list = [item.serialize() for item in characters]
+    response_body = {
+        "msg": "Hello, this is your GET /characters response ",
+        "characters": characters_list
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/users/favorites/planet', methods=['POST'])
+def fav_planets():
+    data = request.get_json()
+    user_id = data['user']
+    planet_id = data['planet']
+    user = db.session.get(User, user_id)
+    planet = db.session.get(Planet, planet_id)
+
+    user.favorite_planet.append(planet)
+    db.session.commit()
+
+    return jsonify(user.serialize()), 200 
+
+@app.route('/users/favorites/characters', methods=['POST'])
+def fav_characters():
+    data = request.get_json()
+    user_id = data['user']
+    character_id = data['characters']
+    user = db.session.get(User, user_id)
+    characters = db.session.get(Characters, character_id)
+
+    user.favorite_characters.append(characters)
+    db.session.commit()
+
+    return jsonify(user.serialize()), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,58 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, Column, Integer, Table, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
+favorite_planet = Table(
+    "favorite_planet",
+    db.Model.metadata,
+    Column("user_id", Integer, ForeignKey("user.id"), primary_key = True),
+    Column("planet_id", Integer, ForeignKey("planet.id"), primary_key = True)
+)
+
+favorite_characters = Table(
+    "favorite_characters",
+    db.Model.metadata,
+    Column("user_id", Integer, ForeignKey("user.id"), primary_key = True),
+    Column("characters_id", Integer, ForeignKey("characters.id"), primary_key = True)
+)
 
 class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True)
+    email = Column(String(120), unique=True, nullable=False)
+    favorite_planet = relationship("Planet", secondary = favorite_planet)
+    favorite_characters = relationship("Characters", secondary = favorite_characters)
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "favorite_planet": [item.serialize() for item in self.favorite_planet],
+            "favorite_characters": [item.serialize() for item in self.favorite_characters]
+            # do not serialize the password, its a security breach
+        }
+
+
+class Planet(db.Model):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(40), unique=True, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            # do not serialize the password, its a security breach
+        }
+
+
+class Characters(db.Model):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
             # do not serialize the password, its a security breach
         }
